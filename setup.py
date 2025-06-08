120796c74cd863d723ff271b7c142d6858d07164
chogamy <gamy0315@daum.net>
2025-06-08 08:28:38 +0000

    add: setup, verison

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name="DecoratorLogger",
-    version="0.1",
-    packages=["decorator_logger"],
+    version="0.0.1",
+    description="Literally Decorator Logger!!",
+    author="chogamy",
+    author_email="gamy0315@gmail.com",
+    url="https://github.com/chogamy/DecoratorLogger",
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=["logger", "decorator", "time", "error"],
+    python_requires=">=3.6",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
